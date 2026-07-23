0d15c884a9ce773ea2b8c12288c16da89a8ea341
Thomas Zijlstra <thomas.zijlstra@accenture.com>
2026-07-23 14:07:06 +0200

    chore: configure GitHub Pages deployment (HashRouter + Vite base path + Actions workflow)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './auth/AuthContext';
 import App from './App';
@@ -22,9 +22,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
